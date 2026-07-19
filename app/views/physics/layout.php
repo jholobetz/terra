0d1fd56da9ded76f9e9a9b0c25f3d905abfdfe63
jholobetz <jholobetz@gmail.com>
2026-07-18 21:55:47 -0600

    feat: switch frontend MathJax library to tex-svg-full.js for font-free offline rendering

--- app/views/physics/layout.php
+++ app/views/physics/layout.php
@@ -12,7 +12,7 @@
 
     <!-- MathJax Static-Safe Configuration -->
     <script src="/js/mathjax_config.js"></script>
-    <script src="/js/lib/tex-chtml-full.js" id="MathJax-script" defer></script>
+    <script src="/js/lib/tex-svg-full.js" id="MathJax-script" defer></script>
     <script src="/js/hub_interactions.js"></script>
     <link rel="stylesheet" href="/css/physics.css">
     <script nonce="<?= $nonce ?>">
